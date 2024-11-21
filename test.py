294edf81ff943c117c28390f1506d3d15f219742
LeonKubota <leon.kubota@student.gyarab.cz>
2024-11-21 11:42:42 +0100

    jdsalk

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def primes_in_range(a, b):
     if not isinstance(a, int) or not isinstance(b, int) or (a < 0) or (b < 0) or (b < a):
         raise ValueError("Error")
     primes = []
-    for i in range(a, b):
+    for i in range(a, b+1):
         if is_prime(i):
             primes.append(i)
     return primes
