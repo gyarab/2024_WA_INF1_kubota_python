3092139b869795ba15efd51a81fdd0104335ab3e
LeonKubota <leon.kubota@student.gyarab.cz>
2024-11-21 11:28:44 +0100

    prvocisla

--- test.py
+++ test.py
@@ -1,24 +1,14 @@
-def fibonacci(n):
-    if isinstance(n, int):
-        if n <= 0:
-            return []
-        elif n == 1:
-            return [0]
-        elif n == 2:
-            return [0, 1]
-        else:
-            fib_sequence = [0, 1]
-            while len(fib_sequence) < n:
-                next_number = fib_sequence[-1] + fib_sequence[-2]
-                fib_sequence.append(next_number)
-            return fib_sequence
+def is_prime(number):
+    if not isinstance(number, int) or number < 0:
+        raise ValueError("Input must be an integer")
     else:
-        return ("invalid input")
+        if number < 2:
+            return False
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                return False
+        return True
 
-def celsius_to_fahrenheit(celsius):
-    fahrenheit = (celsius * 9/5) + 32
-    return fahrenheit
-
-def fahrenheit_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5/9
-    return celsius
+print(is_prime(2))
+print(is_prime(10))
+print(is_prime(13))
