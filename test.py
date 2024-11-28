0e5a8bbfdab104ef9726b3dbe6800073f25828af
LeonKubota <leon.kubota@student.gyarab.cz>
2024-11-28 11:50:54 +0100

    hodiny

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-def vowels_and_consonants(text):
-    if not isinstance(text, str):
-        raise ValueError('Input must be a string')
+def class_and_break_time(start_class, end_class):
+    num_of_classes = end_class - start_class + 1
+    num_of_breaks = num_of_classes - 1
 
-    vowels = 0
-    consonants = 0
-    for char in text:
-        if char.lower() in 'aeěiíoóuůú':
-            vowels += 1
-        elif char.lower() in 'bcčdďfghjklmnpqrřsštťvwxzž':
-            consonants += 1
-    return {'vowels': vowels, 'consonants': consonants}
+    time_of_classes = num_of_classes * 45
+    time_of_breaks = 0
+    
+    for i in range(1, num_of_breaks + 1):
+        if (start_class <= 3 and end_class > 5):
+            time_of_breaks += 10
+        elif (start_class <= 3 and end_class > 3):
+            time_of_breaks += 1000
+
+    return time_of_classes, time_of_breaks
+
+print(class_and_break_time(1, 1))  # (45, 0)
+print(class_and_break_time(1, 2))  # (90, 15)
+print(class_and_break_time(3, 4))  # (90, 20)
+print(class_and_break_time(1, 5))  # (225, 45)
