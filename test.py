371ed8ab39966401095ebe9bd41331e52259dfb7
LeonKubota <leon.kubota@student.gyarab.cz>
2024-11-28 11:06:40 +0100

    fix 3

--- test.py
+++ test.py
@@ -2,8 +2,13 @@ def rotate_array(arr, n):
     if not isinstance(arr, list):
         raise ValueError("Input must be an array")
     
+    if len(arr) == 0:
+        return arr
+    
     if not isinstance(n, int):
         raise ValueError("n must be an integer")
     
     n = n % len(arr)
     return arr[-n:] + arr[:-n]
+
+print(rotate_array([], 2))
