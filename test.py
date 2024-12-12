18834d26a77e6245aed0da3f06fed9f66fcb4b30
LeonKubota <leon.kubota@student.gyarab.cz>
2024-12-12 11:47:18 +0100

    venv

--- test.py
+++ test.py
@@ -1,19 +1,31 @@
-def class_and_break_time(start_class, end_class):
-    num_of_classes = end_class - start_class + 1
-    num_of_breaks = num_of_classes - 1
+import colorsys
 
-    time_of_classes = num_of_classes * 45
-    time_of_breaks = 0
-    
-    for i in range(1, num_of_breaks + 1):
-        if (start_class <= 3 and end_class > 5):
-            time_of_breaks += 10
-        elif (start_class <= 3 and end_class > 3):
-            time_of_breaks += 1000
+def css_color_to_rgb(color):
+    if (color[0] == '#'):
+        # převedení hex na rgb
+        color = color.lstrip('#')
+        r = int(color[0:2], 16)
+        g = int(color[2:4], 16)
+        b = int(color[4:6], 16)
+        return (r, g, b)
+    elif (color[0] == "r"):
+        # rgb do rgb
+        color = color.strip('rgb()')
+        r, g, b = map(int, color.split(','))
+        return (r, g, b)
+    elif (color[0] == "h"):
+        # hsl nebo hsv do rgb
+        color = color.strip('hsl()')
+        h, s, l = map(int, color.split(','))
+        r, g, b = colorsys.hls_to_rgb(h/360, l/100, s/100)
+        return (int(r*255), int(g*255), int(b*255))
+    elif (color[0] == "h"):
+        color = color.strip('hsv()')
+        h, s, v = map(int, color.split(','))
+        r, g, b = colorsys.hsv_to_rgb(h/360, s/100, v/100)
+        return (int(r*255), int(g*255), int(b*255))
 
-    return time_of_classes, time_of_breaks
-
-print(class_and_break_time(1, 1))  # (45, 0)
-print(class_and_break_time(1, 2))  # (90, 15)
-print(class_and_break_time(3, 4))  # (90, 20)
-print(class_and_break_time(1, 5))  # (225, 45)
+print(css_color_to_rgb('#ff0000'))  # (255, 0, 0)
+print(css_color_to_rgb('rgb(255, 0, 0)'))  # (255, 0, 0)
+print(css_color_to_rgb('hsl(0, 100, 50)'))  # (255, 0, 0)
+print(css_color_to_rgb('hsv(0, 100, 50)'))  # (255, 0, 0)
