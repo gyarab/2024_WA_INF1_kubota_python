ba47dc2caabce78508c1c0d4fb04c959dcec501d
LeonKubota <leon.kubota@student.gyarab.cz>
2025-02-11 20:09:45 +0100

    úkol - hotovo?

--- ProjektUkol/AplikaceUkol/views.py
+++ ProjektUkol/AplikaceUkol/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render, get_object_or_404
+from .models import CarSpecifications, Category, Manufacturer
 
-# Create your views here.
+def index(request):
+    cars = CarSpecifications.objects.all()
+    categories = Category.objects.all()
+    manufacturers = Manufacturer.objects.all()
+    return render(request, 'AplikaceUkol/index.html', {'cars': cars, 'categories': categories, 'manufacturers': manufacturers})
+
+def car_detail(request, pk):
+    car = get_object_or_404(CarSpecifications, pk=pk)
+    categories = car.categories.all()
+    manufacturer = car.manufacturer
+    return render(request, 'AplikaceUkol/car_detail.html', {'car': car, 'categories': categories, 'manufacturer': manufacturer})
+
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)  # Získání kategorie podle ID
+    cars = CarSpecifications.objects.filter(categories=category)  # Filtrování aut podle kategorie
+    return render(request, 'AplikaceUkol/category_detail.html', {'category': category, 'cars': cars})
+
+def manufacturer_detail(request, pk):
+    manufacturer = get_object_or_404(Manufacturer, pk=pk)
+    cars = CarSpecifications.objects.filter(manufacturer=manufacturer)
+    return render(request, 'AplikaceUkol/manufacturer_detail.html', {'manufacturer': manufacturer, 'cars': cars})
--- ProjektUkol/ProjektUkol/settings.py
+++ ProjektUkol/ProjektUkol/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'AplikaceUkol.apps.AplikaceukolConfig'
+    'AplikaceUkol.apps.AplikaceukolConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ProjektUkol.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'AplikaceUkol/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "AplikaceUkol/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
