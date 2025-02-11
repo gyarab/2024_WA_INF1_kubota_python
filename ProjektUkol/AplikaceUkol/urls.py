4b102662fe2e0a12f37619c795c9990a052f0712
LeonKubota <leon.kubota@student.gyarab.cz>
2025-02-11 20:41:20 +0100

    done

---
+++ ProjektUkol/AplikaceUkol/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name="AplikaceUkol"
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('cars/<int:pk>/', views.car_detail, name='car_detail'),
+    path('categories/<int:pk>/', views.category_detail, name='category_detail'),
+    path('manufacturers/<int:pk>/', views.manufacturer_detail, name='manufacturer_detail'),
+]
