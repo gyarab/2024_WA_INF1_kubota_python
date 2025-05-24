2b6d06316278391d4e6fbb176b33d8683a1ae173
LeonKubota <leon.kubota@student.gyarab.cz>
2025-05-25 00:28:51 +0200

    formuláře fungují, hurá

--- leon_project/leon_app/models.py
+++ leon_project/leon_app/models.py
@@ -6,3 +6,9 @@ class Project(models.Model):
     short_desc = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField()
     created = models.DateField()
+
+class Feedback(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='feedbacks')
+    name = models.CharField(max_length=50)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
--- leon_project/leon_app/views.py
+++ leon_project/leon_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from .models import Project
+from .models import Project, Feedback
 from django.shortcuts import render, get_object_or_404
+from .forms import FeedbackForm
 
 def index(request):
     projects = Project.objects.order_by('-created')[:3]
@@ -18,4 +19,14 @@ def about(request):
 
 def project(request, pk):
     project = get_object_or_404(Project, pk=pk)
-    return render(request, 'leon_app/project.html', {'project': project})
+
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.project = project
+            feedback.save()
+    else:
+        form = Feedback()
+
+    return render(request, 'leon_app/project.html', {'project': project, 'form': form})
